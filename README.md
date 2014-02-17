b9ca044ff6010a47033096957954f19b2cf14782
suikast42 <S_Vurucu@hotmail.com>
2014-02-17 13:37:23 +0100

    Runs on WF8

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 javaee7PermissionTest
 =====================
+Test the new permission mechanism of Java EE7
--- src/test/java/com/siemag/jpatest/test/ArqtTest.java
+++ src/test/java/com/siemag/jpatest/test/ArqtTest.java
@@ -8,20 +8,48 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.File;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.UnicastRemoteObject;
 
 @RunWith(Arquillian.class)
 public class ArqtTest  {
 
 
+    public class PLCSocketTestImpl extends UnicastRemoteObject {
+
+        protected PLCSocketTestImpl() throws RemoteException {
+        }
+
+
+        protected PLCSocketTestImpl(int port) throws RemoteException {
+            super(port);
+        }
+
+        protected PLCSocketTestImpl(int port,
+                                    RMIClientSocketFactory csf,
+                                    RMIServerSocketFactory ssf)
+                throws RemoteException {
+            super(port, csf, ssf);
+        }
+    }
+
     @Deployment
     public static WebArchive createArchiveTest() {
-        File archive = new File("target/jpatest.war");
+        File archive = new File("target/javaee7PermissionTest.war");
         return ShrinkWrap.createFromZipFile(WebArchive.class, archive);
     }
 
     @Test
-    public void test(){
-        System.out.println("OK");
+    public void test() throws Exception {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new RMISecurityManager());
+        }
+        final String lookupAdress = "//localhost:10097/CRM1_SOCKETSND";
+        Naming.rebind(lookupAdress, new PLCSocketTestImpl());
     }
 
 }
